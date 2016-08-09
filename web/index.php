250f804c7b015280d26d7558dc02040f0f2a2f1b
Sedov Sergey <sedov.sergey@isnlab.com>
2016-08-09 16:50:26 +0300

    add info message

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+	//$info_title = 'Информация'; $info_text = 'Сайт временно недоступен. проводятся технические работы. Время работ 10-20 минут.';
+	//require_once('info.php'); die();
+
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
---
+++ web/info.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?= $info_title ?></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container" style="margin-top: 100px;">
+		<div class="row">
+
+			<div class="col-md-12">
+
+				<div class="alert alert-info">
+					<h4><?= $info_title ?></h4>
+					<p><?= $info_text ?></p>
+					<p>Приносим извинения за предоставленные неудобства.</p>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+</body>
+</html>
