39b485c22f5471018e714695362e504182391adf
Sedov Sergey <sedov.sergey@isnlab.com>
2016-09-26 17:44:47 +0300

    update template

---
+++ commands/CronController.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Created by ISNLab.
+	 * User: Sedov Sergey
+	 * Date: 26.09.2016
+	 * Time: 17:43
+	 * Comment:
+	 */
+	namespace app\commands;
+
+	class CronController extends isnlab\common\commands\CronController
+	{}
+
+
+	?>
